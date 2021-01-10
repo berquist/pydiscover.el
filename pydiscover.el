661dde6a41c20fb80039a320e0b33ffce03a98c9
Eric Berquist <eric.berquist@gmail.com>
2021-01-09 22:29:06 -0500

    initial commit

---
+++ pydiscover.el
@@ -0,0 +1,46 @@
+;;; pydiscover.el --- Discover all possible Python environments on your system -*- lexical-binding: t; -*-
+;;
+;;; Copyright (C) 2021  Free Software Foundation, Inc.
+;;
+;; Author: Eric Berquist <eric.berquist@gmail.com>
+;; Version: 0.0.1
+;; Keywords: extensions
+;; URL: 
+;; Package-Requires: ((emacs "24"))
+;;
+;; This file is not a part of GNU Emacs.
+;;
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+;;
+;;
+;;; Commentary:
+;;
+;; TODO: write commentary
+
+;;; Code:
+
+
+;; TODO: Helper-functions go here (if any)
+
+
+;;;###autoload
+;; TODO: Entry-function goes here
+
+
+(provide 'pydiscover)
+
+;;; pydiscover.el ends here
